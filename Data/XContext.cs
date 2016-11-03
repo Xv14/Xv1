8bd0b5b3f873b10516bb95aed93b28ea03081e15
Bida <mohamedchiheb.bida@esprit.tn>
2016-11-03 08:42:07 +0100

    Added the ARchetype elements

---
+++ Data/XContext.cs
@@ -0,0 +1,24 @@
+﻿using Bida.Data.Configurations;
+using Bida.Data.CustomConventions;
+using Bida.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class XContext : DbContext
+    {
+        public XContext()
+            : base("Name=DefaultConnection")
+        {
+            //Database.SetInitializer<XContext>(new FTFContextInitializer());
+        }
+
+        //public DbSet<User> Clients { get; set; }
+        //public DbSet<Profile> Commandes { get; set; }
+    }
+}
---
+++ XData/XContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XData
+{
+    public class XContext : DbContext
+    {
+        public XContext()
+            : base("Name=DefaultConnection")
+        {
+            //Database.SetInitializer<XContext>(new FTFContextInitializer());
+        }
+
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Profile> Profiles { get; set; }
+    }
+}
+
